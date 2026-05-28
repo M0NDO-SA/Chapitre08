e79265b64d87706dcf4beaaf3a64386689281b00
Gueye-Malick-Simon-Pierre <malickgueye2801@gmail.com>
2026-05-28 11:20:47 +0000

    Troisième Text

--- src/bookstoread/BookShelfSpec.java
+++ src/bookstoread/BookShelfSpec.java
@@ -1,5 +1,6 @@
 package bookstoread;
 
+import static org.junit.jupiter.api.Assertions.assertEqual;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
@@ -12,5 +13,22 @@ public class BookShelfSpec {
         BookShelf shelf = new BookShelf();
         List<String> books = shelf.books();
         asserTrue(books.isEmpty(), () -> "BookShelf should be empty");
+    }
+
+    @Test
+    void bookshelfContainsTwoBooksAdded(){
+        BookShelf shelf = new BookShelf();
+        shelf.add("Effective Java");
+        shelf.add("Code Complete");
+        List<String> books = shelf.books();
+        assertEquals(2, books.size(), () -> "BookShelf should have two books.");
+    }
+
+    @Text
+    public void emptyBookShelfWhenAddIsCalledWithoutBooks(){
+        Bookshelf shelf = new Bookshelf();
+        shelf.add();
+        List<String> books = shelf.books();
+        assertTrue(books.isEmpty(), () -> "BookShelf should be empty");
     }
 }
