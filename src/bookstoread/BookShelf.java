b1fa50baa432450f0ec22704dc1fde829f5eb3f5
Gueye-Malick-Simon-Pierre <malickgueye2801@gmail.com>
2026-06-04 10:24:43 +0000

    arrange modifié

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -1,16 +1,22 @@
 package bookstoread;
-import java.util.Collections;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class BookShelf {
     private final List<String> books = new ArrayList();
 
-    public List<String> books(){
-        return books;
-    }
-
     public void add(String bookToAdd){
         books.add(bookToAdd);
     }
-}
+
+    public List<String> books(){
+        return Collections.unmodifiableList(books);
+    }
+
+    public void add (String...booksToAdd){
+        books.addAll(Arrays.asList(booksToAdd));
+    }
+
+    public List<String> arrange(){
+        return books.stream().sorted().collect(Collectors.toList());
+    }
